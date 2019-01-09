22d605bc34e5043fdcd24bdc743f85a8fdb9b0d0
Kent Friesen <kfreezen@gmail.com>
2019-01-09 17:18:48 -0500

    Latest goproxy wrapper code

--- goproxy/build-internal.bat
+++ goproxy/build-internal.bat
@@ -7,6 +7,11 @@ REM   %GOPROXY_BIN%
 REM   %GOPROXY_GOROOT%
 REM   %GOPROXY_PATH%
 
+SET BUILDMODE=c-shared
+
+IF "%PROXY_OUTPUT_FILE%"=="" SET PROXY_OUTPUT_FILE=proxy.dll
+IF "%PROXY_OUTPUT_FILE%"=="proxy.exe" SET BUILDMODE=exe
+
 set OLD_GOROOT=%GOROOT%
 set OLD_GOPATH=%GOPATH%
 
@@ -17,7 +22,7 @@ set GOPATH=%GOPROXY_GOPATH%
 
 mkdir %GOPROXY_BIN%
 
-%GOPROXY_PATH%\go build -x -ldflags "-s -w" --buildmode=c-shared -o %GOPROXY_BIN%\proxy.dll
+%GOPROXY_PATH%\go build -ldflags "-s -w" --buildmode=%BUILDMODE% -o %GOPROXY_BIN%\%PROXY_OUTPUT_FILE%
 
 set GOROOT=%OLD_GOROOT%
 set GOPATH=%OLD_GOPATH%
